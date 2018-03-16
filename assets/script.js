d80213fc418a250e54b973e9df5e6329fb37bc7b
Lee <32332479+mynar7@users.noreply.github.com>
2018-03-16 10:58:27 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -89,7 +89,7 @@ function displayQ (num) {
 //this shows the answers by changing the colors :D
 function displayA(str) {
     $('li#correct').css("color", "green").off("click");
-    $('li#incorrect').css("color", "red").off("click").fadeOut(2000);
+    $('li#incorrect').css("color", "red").off("click").fadeOut(2000).remove();
     setTimeout(function(){
     $('<p>').html(str).hide().appendTo('#answers').fadeIn(2000);
     }, 2000);
